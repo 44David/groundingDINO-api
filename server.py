eb050fa66c6f5cf1175ec790d2903b967551349c
44David <david.soane@outlook.com>
2025-02-19 18:23:13 -0600

    Added owlViT model

--- server.py
+++ server.py
@@ -3,11 +3,14 @@ import requests
 import torch
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
+import requests
+from transformers import OwlViTProcessor, OwlViTForObjectDetection
 
+processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
 
 app = FastAPI()
 
-@app.get("/prediction")
+@app.get("/prediction/groundingdino")
 def predict():
 		model_id = "IDEA-Research/grounding-dino-base"
 		device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,3 +37,41 @@ def predict():
 		)
   
 		return {"output": results}
+
+@app.get("/prediction/owl-vit")
+def predict():
+	model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
+
+	url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+
+	image = Image.open(requests.get(url, stream=True).raw)
+
+	text_labels = [["a photo of a cat", "a photo of a dog"]]
+
+	inputs = processor(text=text_labels, images=image, return_tensors="pt")
+
+	outputs = model(**inputs)
+
+	# Target image sizes (height, width) to rescale box predictions [batch_size, 2]
+
+	target_sizes = torch.tensor([(image.height, image.width)])
+
+	# Convert outputs (bounding boxes and class logits) to Pascal VOC format (xmin, ymin, xmax, ymax)
+
+	results = processor.post_process_grounded_object_detection(
+
+		outputs=outputs, target_sizes=target_sizes, threshold=0.1, text_labels=text_labels
+
+	)
+
+	# Retrieve predictions for the first image for the corresponding text queries
+
+	result = results[0]
+
+	boxes, scores, text_labels = result["boxes"], result["scores"], result["text_labels"]
+
+	for box, score, text_label in zip(boxes, scores, text_labels):
+
+		box = [round(i, 2) for i in box.tolist()]
+
+		print(f"Detected {text_label} with confidence {round(score.item(), 3)} at location {box}")
