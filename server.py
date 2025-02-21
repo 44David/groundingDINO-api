4df2b9639eca565e7383c3be10a3704c3ea7345d
44David <david.soane@outlook.com>
2025-02-21 11:22:31 -0600

    Modified API to accept and use request data

--- server.py
+++ server.py
@@ -1,25 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import requests
 import torch
 from PIL import Image
-from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
-import requests
-from transformers import OwlViTProcessor, OwlViTForObjectDetection
-
-processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
+from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection, OwlViTProcessor, OwlViTForObjectDetection
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+	CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/prediction/groundingdino")
-def predict():
+async def predict(req: Request):
+    
+		req_json = await req.json()
+		req_url = req_json.s3Url
+    
 		model_id = "IDEA-Research/grounding-dino-base"
 		device = "cuda" if torch.cuda.is_available() else "cpu"
 
 		processor = AutoProcessor.from_pretrained(model_id)
 		model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
 
-		image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-		image = Image.open(requests.get(image_url, stream=True).raw)
+		image = Image.open(requests.get(req_url, stream=True).raw)
 		# Check for cats and remote controls
 		# VERY important: text queries need to be lowercased + end with a dot
 		text = "a cat. a remote control."
@@ -38,40 +51,34 @@ def predict():
   
 		return {"output": results}
 
-@app.get("/prediction/owl-vit")
-def predict():
+@app.post("/prediction/owl-vit")
+async def predict(req: Request):
+
+	req_json = await req.json()
+	req_url = req_json.s3Url
+		
+	processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
 	model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
 
-	url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-
-	image = Image.open(requests.get(url, stream=True).raw)
-
+	image = Image.open(requests.get(req_url, stream=True).raw)
 	text_labels = [["a photo of a cat", "a photo of a dog"]]
 
 	inputs = processor(text=text_labels, images=image, return_tensors="pt")
-
 	outputs = model(**inputs)
 
 	# Target image sizes (height, width) to rescale box predictions [batch_size, 2]
-
 	target_sizes = torch.tensor([(image.height, image.width)])
 
 	# Convert outputs (bounding boxes and class logits) to Pascal VOC format (xmin, ymin, xmax, ymax)
-
 	results = processor.post_process_grounded_object_detection(
-
 		outputs=outputs, target_sizes=target_sizes, threshold=0.1, text_labels=text_labels
-
 	)
 
 	# Retrieve predictions for the first image for the corresponding text queries
-
 	result = results[0]
 
 	boxes, scores, text_labels = result["boxes"], result["scores"], result["text_labels"]
 
 	for box, score, text_label in zip(boxes, scores, text_labels):
-
 		box = [round(i, 2) for i in box.tolist()]
-
 		print(f"Detected {text_label} with confidence {round(score.item(), 3)} at location {box}")
